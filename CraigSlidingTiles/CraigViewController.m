872fabe33cdb262a6aeebf4fe74ac4fd696ed857
Event <event@nyb-loan37-lm.(none)>
2013-07-11 13:26:01 -0700

    some polish

--- CraigSlidingTiles/CraigViewController.m
+++ CraigSlidingTiles/CraigViewController.m
@@ -7,7 +7,10 @@
 //
 
 #import <QuartzCore/QuartzCore.h>
+#import <AudioToolbox/AudioToolbox.h>
 #import "CraigViewController.h"
+
+#define TILESIZE 73.0f
 
 @implementation CraigViewController
 
@@ -22,16 +25,30 @@
 }
 
 - (UIButton *) addTileWithValue: (int)value atPosition: (int)position {
-    UIButton *myTile;
+    UIButton *myTile = [UIButton buttonWithType:UIButtonTypeCustom];
+
     if(value > 0) {
-        myTile = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+
+        // Draw a custom gradient
+        CAGradientLayer *btnGradient = [CAGradientLayer layer];
+        btnGradient.frame = CGRectMake(0, 0, TILESIZE, TILESIZE);
+        btnGradient.colors = [NSArray arrayWithObjects:(id)[[UIColor colorWithRed:102.0f / 255.0f green:102.0f / 255.0f blue:102.0f / 255.0f alpha:1.0f] CGColor],
+                              (id)[[UIColor colorWithRed:51.0f / 255.0f green:51.0f / 255.0f blue:51.0f / 255.0f alpha:1.0f] CGColor],
+                              nil];
+        [myTile.layer insertSublayer:btnGradient atIndex:0];
+
+        CALayer *btnLayer = [myTile layer];
+        [btnLayer setMasksToBounds:YES];
+        [btnLayer setCornerRadius:10.0f];
+
+        [myTile setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+
     } else {
-        myTile = [UIButton buttonWithType:UIButtonTypeCustom];
         myTile.backgroundColor = [UIColor clearColor];
         myTile.layer.borderColor = [UIColor clearColor].CGColor;
+        [myTile setTitleColor:[UIColor clearColor] forState:UIControlStateNormal];
     }
     [myTile setTitle:[NSString stringWithFormat:@"%d", value] forState:UIControlStateNormal];
-    [myTile setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
     myTile.titleLabel.font = [UIFont boldSystemFontOfSize:30.0];
     myTile.frame = [self getRectForObjectAtIndex: position];
     [myTile addTarget:self action:@selector(tileTapped:) forControlEvents:UIControlEventTouchUpInside];
@@ -80,8 +97,9 @@
 }
 
 - (BOOL) checkWin {
+    // check for order
     for (int i = 0; i < 16; i++) {
-        int posValue = [[gameValues objectAtIndex:i] integerValue];
+        int posValue = [[gameValues objectAtIndex:i] intValue];
         
         if(i != posValue) return NO;
     }
@@ -95,6 +113,8 @@
 }
 
 - (void) tileTapped:(id)sender {
+    if(gameInProgress == NO) return;
+    
     NSString *titleText = ((UIButton *)sender).titleLabel.text;
     int titleInt = [titleText intValue];
     int valueIndex = [gameValues indexOfObjectPassingTest:^(id obj, NSUInteger idx, BOOL *stop) {
@@ -105,7 +125,7 @@
             return NO;
         }
     }];
-    NSLog(@"you tapped tile with value %@ which is at position %d", titleText, valueIndex);
+    //NSLog(@"you tapped tile with value %@ which is at position %d", titleText, valueIndex);
     
     if(titleInt > 0) {
         if(valueIndex > 3) {
@@ -152,11 +172,13 @@
     zeroButton.hidden = YES;
     UIButton *movedButton = [gameViews objectAtIndex:toIndex];
     
+    [self playMoveSound];
     [UIView animateWithDuration:0.3 animations:^{
         movedButton.frame = [self getRectForObjectAtIndex:toIndex];
     } completion:^(BOOL finished){
         zeroButton.frame = [self getRectForObjectAtIndex:fromIndex];
         zeroButton.hidden = NO;
+        [self checkWin];
     }];
 }
 
@@ -164,7 +186,21 @@
     int row = (int)floor(index / 4);
     int col = index % 4;
     
-    return CGRectMake(col * 75, row * 75, 75, 75);
+    return CGRectMake(col * 75, row * 75, TILESIZE, TILESIZE);
+}
+
+- (void) playMoveSound {
+    SystemSoundID audioEffect;
+    
+    NSString *path  = [[NSBundle mainBundle] pathForResource:@"hit-01" ofType:@"wav"];
+    NSURL *pathURL = [NSURL fileURLWithPath : path];
+    AudioServicesCreateSystemSoundID((__bridge CFURLRef) pathURL, &audioEffect);
+    AudioServicesPlaySystemSound(audioEffect);
+/*
+    // call the following function when the sound is no longer used
+    // (must be done AFTER the sound is done playing)
+    AudioServicesDisposeSystemSoundID(audioEffect);
+*/
 }
 
 @end
